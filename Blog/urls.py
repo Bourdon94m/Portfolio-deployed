e458f450616c240cea901b0a52166a9a78e746e5
Bourdon94m <105060610+Bourdon94m@users.noreply.github.com>
2024-04-01 21:10:51 +0200

    initial commit

---
+++ Blog/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+from django.urls import path, include
+from .views import index, detail, ressources_page
+
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('detail/<str:slug>/', detail, name='detail'),
+    path('ressources/', ressources_page, name='ressources'),
+]
